c8580a303f229535200353b57ef7c87ac7f7220e
Renato Alves Soares <renatoguara2020@gmail.com>
2022-11-25 10:47:03 -0300

    ok

--- app_send_mail/processa_envio.php
+++ app_send_mail/processa_envio.php
@@ -54,7 +54,7 @@
 			$mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
 			$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
 			$mail->Username   = 'renatoguara2020@gmail.com';                     //SMTP username
-			$mail->Password   = 'gpxy';                               //SMTP password
+			$mail->Password   = 'gpx';                               //SMTP password
 			$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
 			$mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
